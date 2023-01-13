c6da217628f96d0eb39567b07824f768d342551f
valdiev <valdiev@live.fr>
2023-01-13 09:40:34 +0100

    Fix conflicts

---
+++ controllers/ActorController.php
@@ -0,0 +1,49 @@
+<?php
+namespace controllers;
+
+use controllers\base\WebController;
+use models\FilmsModele;
+use utils\Template;
+
+class FilmController extends WebController
+{
+    function __construct()
+    {
+        $this->filmModele = new FilmsModele();
+    }
+
+    function film($id) {
+        $filmModele = new FilmsModele();
+        try{
+            $leFilm = $filmModele->getByFilmId($id);
+        }
+        catch (\Throwable $e){
+            var_dump($e);
+            exit;
+        }
+        return Template::render(
+            "views/single_film/single_film.php",
+            array("leFilm" => $leFilm)
+        );
+    }
+
+    function liste($page = 0): string
+    {
+        $films = $this->filmModele->liste(10, $page);
+        // if($_SESSION) {
+            return Template::render(
+                "views/liste_films/liste_films.php",
+                array("page" => $page, "films" => $films)
+            );
+        // } else {
+        //     header('location: /');
+        // }
+    }
+
+    function ajouter($page = 0): string
+    {
+        return Template::render(
+            "views/liste_films/ajouter_films.php"
+        );
+    }
+}
