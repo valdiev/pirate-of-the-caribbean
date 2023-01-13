1689355d234b6271e24062d8289b28ac1465fcd9
valdiev <valdiev@live.fr>
2023-01-13 11:35:03 +0100

    Add / delete feature disable if not login

--- views/liste_actors/liste_actors.php
+++ views/liste_actors/liste_actors.php
@@ -1,9 +1,14 @@
-<a href="acteurs/ajouter">Ajouter un acteur</a>
+<?php if($_SESSION) { ?>
+    <a href="acteurs/ajouter">Ajouter un acteur</a>
+<?php } ?>
+
 <?php
 /** @var Actor[] $clients */
 foreach ($actors as $a) { ?>
     <div class="card m-3 p-3">
         <h3><?= $a->generalInfo() ?></h3>
-        <a href="/acteurs/remove/<?= $a->getId() ?>">Remove film</a>
+        <?php if($_SESSION) { ?>
+            <a href="/acteurs/remove/<?= $a->getId() ?>">Remove actor</a>
+        <?php } ?>
     </div>
 <?php } ?>
