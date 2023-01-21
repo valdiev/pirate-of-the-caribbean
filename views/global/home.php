f1fc082d714539c0bc222e0962b960e7a549c770
theoBIGBIZYOU <theo@bigbizyou.agency>
2023-01-21 01:12:17 +0100

    add background HP

--- views/global/home.php
+++ views/global/home.php
@@ -1,19 +1,7 @@
-<div class="container mt-5">
-    <div class="row">
-        <div class="card">
-            <div class="card-body">
-                <h3>Site de démonstration</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, commodi delectus eaque eos esse ex illum laboriosam laborum modi, molestias necessitatibus non provident quidem quis repellat soluta tempore, veritatis vitae.
-                </p>
-
-                <ul>
-                    <li><a href="/exemple">Lien d'exemple 1</a></li>
-                    <li><a href="/exemple2/votre_parametre">Lien d'exemple 2</a></li>
-                </ul>
-
-                <div class="text-center">Page générée le <?= $date ?></div>
-            </div>
+<section class="homepage">
+    <div class="homepage_header">
+        <div class="homepage_header_image">
+            <img src="public/img/pirate-bg3.jpg" alt="">
         </div>
     </div>
-</div>
+</section>
