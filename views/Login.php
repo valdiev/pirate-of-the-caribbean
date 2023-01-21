32f454366f7b0701828a659ab3e3faa3685b29c2
valdiev <valdiev@live.fr>
2023-01-21 16:18:37 +0100

    stylizing login page

--- views/Login.php
+++ views/Login.php
@@ -1,9 +1,12 @@
-<h1>Pour l'exemple vous pouvez utiliser le compte :</h1>
-<h2>admin / admin</h2>
-<form name="form1" method="post" action="login/run">
-    <label for="uname">User Name</label>
-    <input type="text" id="user_name" name="user_name"><br><br>
-    <label for="pwd">Password</label>
-    <input type="password" id="password" name="password"><br><br>
-    <input name="submit" type="submit" id="submit" value="Login"><br>
-</form>
+<section id="login__content">
+    <div id="login__content-form">
+        <h1>Connectez-vous pour administrer ce site</h1>
+        <form name="form1" method="post" action="login/run">
+            <label for="uname">Identifiant</label>
+            <input type="text" id="user_name" name="user_name"><br><br>
+            <label for="pwd">Mot de passe</label>
+            <input type="password" id="password" name="password"><br><br>
+            <button type="submit" class="pirate_button">Se connecter</button>
+        </form>
+    </div>
+</section>
