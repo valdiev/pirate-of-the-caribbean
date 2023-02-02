8a012d5dbf25437f2351821b67394c2e5f8d51a5
valdiev <valdiev@live.fr>
2023-02-02 23:04:08 +0100

    Add responsive scss

--- public/js/list_film.js
+++ public/js/list_film.js
@@ -1,8 +1,14 @@
-if(document.querySelector(".listFilm") || document.querySelector(".homepage")) {
-    var offsetStart = 0;
-    var offsetEnd = 0;
-
-    window.addEventListener('scroll', () => {
-        document.documentElement.style.setProperty('--scroll', (window.pageYOffset - offsetStart) / (document.body.offsetHeight - offsetStart - offsetEnd - window.innerHeight));
-    }, false);
+function scrollToSlide() {
+    if(window.innerWidth > 1024) {
+        if(document.querySelector(".listFilm") || document.querySelector(".homepage") && window.matchMedia("(min-width: 1024px)").matches) {
+            var offsetStart = 0;
+            var offsetEnd = 0;
+        
+            window.addEventListener('scroll', () => {
+                document.documentElement.style.setProperty('--scroll', (window.pageYOffset - offsetStart) / (document.body.offsetHeight - offsetStart - offsetEnd - window.innerHeight));
+            }, false);
+        }
+    }
 }
+
+scrollToSlide();
